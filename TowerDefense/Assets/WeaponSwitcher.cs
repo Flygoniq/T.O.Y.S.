6c1e9f7e716bbc75fef03c16287c9d13750c175a
iwong3 <ivanwong15@gmail.com>
2017-04-11 21:06:55 -0400

    implemented functionality to switch weapons

---
+++ TowerDefense/Assets/WeaponSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour {
+
+    public GameObject sword;
+    public GameObject gun;
+
+	// Use this for initialization
+	void Start () {
+        gun.SetActive(true);
+        sword.SetActive(false);
+            
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("1"))
+        {
+            gun.SetActive(true);
+            sword.SetActive(false);
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            gun.SetActive(false);
+            sword.SetActive(true);
+        }
+    }
+}
