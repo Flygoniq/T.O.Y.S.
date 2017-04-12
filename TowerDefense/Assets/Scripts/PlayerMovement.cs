d3d77d812cb7c240420c5551f68f72235ca91169
iwong3 <ivanwong15@gmail.com>
2017-04-12 05:45:29 -0400

    pushes for alan

--- TowerDefense/Assets/Scripts/PlayerMovement.cs
+++ TowerDefense/Assets/Scripts/PlayerMovement.cs
@@ -27,10 +27,10 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
 
 		// WASD forward/back & left/right movement is stored in "direction"
-		direction = transform.rotation * new Vector3( Input.GetAxis("Horizontal") , 0, Input.GetAxis("Vertical") );
+		direction = transform.rotation * new Vector3( Input.GetAxis("Horizontal") , 0, Input.GetAxis("Vertical"));
 
-		// This ensures that we don't move faster going diagonally
-		if(direction.magnitude > 1f) {
+        // This ensures that we don't move faster going diagonally
+        if (direction.magnitude > 1f) {
 			direction = direction.normalized;
 		}
 
